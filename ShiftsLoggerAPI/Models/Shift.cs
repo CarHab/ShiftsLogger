e3b415cd86cca104bc2c403f9012ca95a375cd48
Carlos Eduardo Habib Baptista Lima <carloshabib03@gmail.com>
2023-03-09 20:58:36 -0300

    Add project files.

---
+++ ShiftsLoggerAPI/Models/Shift.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShiftsLoggerAPI.Models;
+
+public class Shift
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime Start { get; set; }
+
+    [Required]
+    public DateTime End { get; set; }
+
+    [Required]
+    public Worker? Worker { get; set; }
+}
---
+++ ShiftsLoggerAPI/Models/ShiftsLoggerContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShiftsLoggerAPI.Models;
+
+public class ShiftsLoggerContext : DbContext
+{
+    public DbSet<Worker> Workers { get; set; }
+    public DbSet<Shift> Shifts { get; set; }
+
+    public ShiftsLoggerContext(DbContextOptions<ShiftsLoggerContext> options)
+            : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ShiftsLogger;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+    }
+}
